6ccc677d9b184cf13ce0e2fc73c0afcc8e9fa89f
Jennifer <jenndestes@gmail.com>
2021-06-22 22:41:58 -0500

    Created render method and uncommented out app import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,10 @@ import { createSore, applyMiddleware, compose } from 'redux'
 import { provider } from 'react-redux'
 import thunk from 'reux-thunk'
 // gets rid of line of code starting with window.__
-
 import { composeWithDevTools } from 'redux-devtools-extension'
+import App from './App'
+
 // import './index.css'
-// import App from './App'
 const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
