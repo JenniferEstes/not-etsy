2ddc5e2b82927297304d811134929c6bc0355623
Jennifer <jenndestes@gmail.com>
2021-06-24 17:39:27 -0500

    Set up businessList in Redux, with cohort lead's help

---
+++ frontend/src/components/BusinessList.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector} from "react-redux";
+import { fetchBusinesses } from "../actions/businessActions";
+
+export default function BusinessList() {
+    const busList = useSelector( state => state.businesses)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchBusinesses())
+    }, [])
+
+    return (
+        <div>
+            <ul>
+                {busList.map(b => <li>(b.name)</li>)}
+            </ul>
+        </div>
+    )
+}
