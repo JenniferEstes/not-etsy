590b33c71708b5682d4c70948171d76990817186
Jennifer <jenndestes@gmail.com>
2021-06-24 02:51:39 -0500

    Added 5 routes

---
+++ frontend/src/components/Router.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// switch allows navigation between routes; finds exact route and first route that matches
+import { Switch, Route } from 'react-router-dom'
+import Home from './Home'
+import BusinessList from './BusinessList'
+import CategoryList from './CategoryList'
+import FavoriteList from './FavoriteList'
+import ProductList from './ProductList'
+
+export const Router = () => {
+    return (
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/businesses' component={BusinessList} />
+            <Route path='/categories' component={CategoryList} />
+            <Route path='/favorites' component={FavoriteList} />
+            <Route path='/products' component={ProductList} />
+        </Switch>
+    )
+}
+
+// stateless component
