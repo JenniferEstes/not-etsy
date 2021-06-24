ce233373d8eea60f90788ff19acc6c7067e92449
Jennifer <jenndestes@gmail.com>
2021-06-24 17:40:04 -0500

    Created businessReducer function

--- frontend/src/reducers/businessReducer.js
+++ frontend/src/reducers/businessReducer.js
@@ -1,1 +1,13 @@
 const initialState = []
+
+export default function businessReducer(state = initialState, action){
+    switch(action.type){
+        case 'FETCH_BUSINESSES':
+            return {
+                ...state,
+                businesses: action.payload
+            }
+        default:
+            return state
+    }
+}
