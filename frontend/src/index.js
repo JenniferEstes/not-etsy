d4216952ce498a7c5df2b1623718d746dc1edc6b
Jennifer <jenndestes@gmail.com>
2021-06-23 20:24:28 -0500

    imported rootReducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,10 @@ import thunk from 'redux-thunk'
 // gets rid of line of code starting with window.__
 import { composeWithDevTools } from 'redux-devtools-extension'
 import './App'
-import productList from './components/productList'
+import categoryList from './components/categoryList'
 import favoriteList from './components/favoriteList'
 import productList from './components/productList'
-
-
+import rootReducer from './reducers/rootReducer'
 // import './index.css'
 const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
 
