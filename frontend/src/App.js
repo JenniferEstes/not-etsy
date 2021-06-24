92978dd94bf4724f9361741b4d6d5493ad6c64b5
Jennifer <jenndestes@gmail.com>
2021-06-24 17:38:04 -0500

    Imported product and business lists and returned those lists

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './App.css'
+import { Router } from './components/Router'
+import ProductList from './components/ProductList'
+import BusinessList from './components/BusinessList'
+
 
 function App() {
   return (
       <div>
-          App.js
+          <ProductList />
+          <BusinessList />
+          <Router />
       </div>
   )
 }
 
 export default App;
+// stateless component
