8255ab864def97b954ab27819cabb8b68459debc
Jennifer <jenndestes@gmail.com>
2021-06-24 20:26:43 -0500

    Imported CategoryList and returned it in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import './App.css'
 import { Router } from './components/Router'
 import ProductList from './components/ProductList'
 import BusinessList from './components/BusinessList'
-
+import CategoryList from './components/CategoryList'
 
 function App() {
   return (
       <div>
           <ProductList />
           <BusinessList />
+          <CategoryList />
           <Router />
       </div>
   )
