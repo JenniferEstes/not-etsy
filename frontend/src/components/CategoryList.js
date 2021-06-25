7a4c7fde58d9256722eb652f143858c426733674
Jennifer <jenndestes@gmail.com>
2021-06-24 21:19:00 -0500

    Corrected misspelling of fetchCategories in CategoryList.js

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector} from 'react-redux'
-import { fetchCategories } from '../actions/categoryActions'
+import { fetchCategories } from '../actions/categoryAction'
 
 export default function CategoryList() {
     const categoryList = useSelector( state => state.categories)
