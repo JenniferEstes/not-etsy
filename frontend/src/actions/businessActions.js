f71a771c70dd92e8e11b0df1b3045b7c8c473300
Jennifer <jenndestes@gmail.com>
2021-06-24 20:19:31 -0500

    Added type

--- frontend/src/actions/businessActions.js
+++ frontend/src/actions/businessActions.js
@@ -2,6 +2,6 @@ export const fetchBusinesses = () => {
     return (dispatch) => {
         fetch('http://localhost:3001/businesses')
             .then(resp => resp.json())
-            .then(businesses => dispatch({ type: 'FETCH_PRODUCTS', payload: businesses }))
+            .then(businesses => dispatch({ type: 'FETCH_BUSINESSES', payload: businesses }))
     }
 }
