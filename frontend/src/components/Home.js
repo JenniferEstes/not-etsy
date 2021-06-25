eacba6a92030fd7157a151c09a1d393ecd06b141
Jennifer <jenndestes@gmail.com>
2021-06-25 15:35:23 -0500

    Imported react-bootstrap and ReactImageAppear

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,10 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, {Component} from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import { Link } from 'react-router-dom'
+import ReactImageAppear from 'react-image-appear'
 
 export default function Home() {
 
