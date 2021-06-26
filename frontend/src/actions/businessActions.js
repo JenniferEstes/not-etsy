b6b842b78819648a87fd99f7e0b2d957a97eb724
Jennifer <jenndestes@gmail.com>
2021-06-26 01:10:39 -0500

    Corrected port number in fetchBusinesses endpoint to 3000 from 3001

--- frontend/src/actions/businessActions.js
+++ frontend/src/actions/businessActions.js
@@ -1,6 +1,6 @@
 export const fetchBusinesses = () => {
     return (dispatch) => {
-        fetch('http://localhost:3001/businesses')
+        fetch('http://localhost:3000/businesses')
             .then(resp => resp.json())
             .then(businesses => dispatch({ type: 'FETCH_BUSINESSES', payload: businesses }))
     }
