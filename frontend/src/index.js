90619b28e48c930df2cf3d4d029f50e0afd07cc1
Jennifer <jenndestes@gmail.com>
2021-06-25 23:48:17 -0500

    Added in React.StrictMode to render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -10,16 +9,19 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import rootReducer from './reducers/rootReducer'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const store = createStore(
     rootReducer,
     composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    </React.StrictMode>,
     document.getElementById('root')
 )
