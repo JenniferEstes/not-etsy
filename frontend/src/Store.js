be308dafaf6d19960715ecf1873085ecd370af64
Jennifer <jenndestes@gmail.com>
2021-06-25 21:56:06 -0500

    Imported businessReducer

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,1 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import businessReducer from "./reducers/businessReducer";
+
+const store = configureStore({
+    reducer: {
+        business: businessReducer
+    },
+})
+
+export default store
