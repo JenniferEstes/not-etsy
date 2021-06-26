c02204a0631d5fb72766ee09d3586a29c753b9b5
Jennifer <jenndestes@gmail.com>
2021-06-25 21:56:47 -0500

    Imported categoryReducer

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import businessReducer from "./reducers/businessReducer";
+import categoryReducer from "./reducers/categoryReducer";
 
 const store = configureStore({
     reducer: {
-        business: businessReducer
+        business: businessReducer,
+        category: categoryReducer,
     },
 })
 
