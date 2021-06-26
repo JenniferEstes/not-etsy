cc2464891f1ecd85fb4fb5ea75ddcdacf428f748
Jennifer <jenndestes@gmail.com>
2021-06-25 23:20:13 -0500

    Imported favoriteReducer

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import businessReducer from "./reducers/businessReducer";
 import categoryReducer from "./reducers/categoryReducer";
+import favoriteReducer from "./reducers/favoriteReducer";
 
 const store = configureStore({
     reducer: {
         business: businessReducer,
         category: categoryReducer,
+        favorite: favoriteReducer,
     },
 })
 
