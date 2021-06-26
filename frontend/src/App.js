65afc55d297ca1a3bcc640b331a3ef8b3712c70a
Jennifer <jenndestes@gmail.com>
2021-06-26 00:36:04 -0500

    Removed imported and rendered lists so all the data doesn't show on the homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import './App.css'
 import { Router } from './components/Router'
-import ProductList from './components/ProductList'
-import BusinessList from './components/BusinessList'
-import CategoryList from './components/CategoryList'
 
 function App() {
   return (
       <div>
-          <ProductList />
-          <BusinessList />
-          <CategoryList />
           <Router />
       </div>
   )
