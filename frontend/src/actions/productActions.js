5576764576b556411f85c76ac0d28cdf10216055
Jennifer <jenndestes@gmail.com>
2021-06-26 00:38:36 -0500

    Changed the port number in method to 3000 from 3001 because

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,6 +1,6 @@
 export const fetchProducts = () => {
     return (dispatch) => {
-        fetch('http://localhost:3001/products')
+        fetch('http://localhost:3000/products')
             .then(resp => resp.json())
             .then(products => dispatch({ type: 'FETCH_PRODUCTS', payload: products }))
     }
