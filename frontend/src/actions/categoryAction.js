e7bc54d6255712de225c751ccba16df2e5673157
Jennifer <jenndestes@gmail.com>
2021-06-26 00:54:59 -0500

    Corrected port number in fetchCategories endpoint to 3000 from 3001

--- frontend/src/actions/categoryAction.js
+++ frontend/src/actions/categoryAction.js
@@ -1,6 +1,6 @@
 export const fetchCategories = () => {
     return (dispatch) => {
-        fetch('http://localhost:3001/categories')
+        fetch('http://localhost:3000/categories')
             .then(resp => resp.json())
             .then(categories => dispatch({ type: 'FETCH_CATEGORIES', payload: categories }))
     }
