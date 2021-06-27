5796f618c21f8f3819a12b664299debeb602023d
Jennifer <jenndestes@gmail.com>
2021-06-27 00:11:09 -0500

    Added an 's' to 'FETCH_FAVORITE'

--- frontend/src/Reducers/favoriteReducer.js
+++ frontend/src/Reducers/favoriteReducer.js
@@ -1,8 +1,9 @@
+// reducers must be pure and not mutate original state
 const initialState = []
 
 export default function favoriteReducer(state = initialState, action){
     switch(action.type){
-        case 'FETCH_FAVORITE':
+        case 'FETCH_FAVORITES':
             return {
                 ...state,
                 favorites: action.payload
