4a5e962e0af3eeff3fa13e1954b4959edf4d92c4
Jennifer <jenndestes@gmail.com>
2021-06-27 00:16:36 -0500

    Added TOGGLE_FAVORITE case to productReducer

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,13 +1,26 @@
 const initialState = []
 
-export default function productReducer(state = initialState, action){
-    switch(action.type){
+export default function productReducer(state = initialState, action) {
+    switch (action.type) {
         case 'FETCH_PRODUCTS':
             return {
                 ...state,
                 products: action.payload
             }
-            default:
-                return state
+        case 'TOGGLE_FAVORITE':
+            return {
+                ...state,
+                products: state.products.map((product) => {
+                    // If the id is for the changed product, return the updated one.
+                    if (product.id === action.payload.id) {
+                        return action.payload
+                    } else {
+                        // Otherwise return the existing one.
+                        return product
+                    }
+                })
+            }
+        default:
+            return state
     }
 }
