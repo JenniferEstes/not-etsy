c63a22ce30464d9bac6f7b48e3d72a5309b41d80
Jennifer <jenndestes@gmail.com>
2021-06-26 23:48:53 -0500

    Imported and returned navbar in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/NavBar'
 import { Router } from './components/Router'
 
 function App() {
   return (
       <div>
+          <Navbar />
           <Router />
       </div>
   )
