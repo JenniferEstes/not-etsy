ac589982eb2e27f85325196b0b1adbe25584dcff
Jennifer <jenndestes@gmail.com>
2021-06-29 01:14:19 -0500

    Cleaned up quotes to make them consistent

--- frontend/src/components/BusinessList.js
+++ frontend/src/components/BusinessList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector} from "react-redux";
-import { fetchBusinesses } from "../actions/businessActions";
+import { useDispatch, useSelector} from 'react-redux'
+import { fetchBusinesses } from '../actions/businessActions'
 import Card from 'react-bootstrap/Card'
 
 export default function BusinessList() {
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHeart as fasHeart} from "@fortawesome/free-solid-svg-icons";
-import {faHeart as farHeart} from "@fortawesome/free-regular-svg-icons";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHeart as fasHeart} from '@fortawesome/free-solid-svg-icons'
+import {faHeart as farHeart} from '@fortawesome/free-regular-svg-icons'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 
