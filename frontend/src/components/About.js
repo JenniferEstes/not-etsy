ada4ae4569c3a633e2cd4353bd2a43a74d94d0fa
Jennifer <jenndestes@gmail.com>
2021-06-28 22:40:10 -0500

    Put about paragraph into a card

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 
-function fixedBottom() {
+function About() {
 
     return (
         <>
-        <Navbar bg="light" expand="md" className="mb-7 shadow-sm" fixed="bottom">
-            <Navbar.Brand>Not Etsy</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-            </Nav>
-        </Navbar>
+            <Card className="category-container">
+                <Card.Body>
+                    <Card.Title>About Not Etsy</Card.Title>
+                    <Card.Text>
+                        Supporting small businesses is a passion of mine. When I have free time, I enjoy browsing through the popular e-commerce site, Etsy, where mostly small businesses sell handmade or vintage items. One main part of this process is favoriting items I may want to purchase at a later date.
+                        Not Etsy is a single page application (SPA) that uses seed data to show how a customer can browse products, businesses, categories, and their favorite products on site like Etsy.
+                        Fun Fact: The seed data came directly from my favorite items on Etsy right now.
+                        You can find the link to the favorites page on the same navigation bar you used to reach this page.
+                        Feel free to favorite and un-favorite as much as you'd like!
+                        I hope this application inspires you to support more small businesses, whether it be in your local community or online.
+                        <br/><br/>
+                        Best Wishes,<br/>
+                        Jennifer Estes
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </>
     )
 }
 
-export default fixedBottom
+export default About
 
 // stateless component
