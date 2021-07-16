7d67ac0d7ef24f0974f58dd5e64d98558e950471
Jennifer <jenndestes@gmail.com>
2021-07-16 01:13:15 -0500

    Added commented out notes for educational purposes

--- frontend/src/Reducers/favoriteReducer.js
+++ frontend/src/Reducers/favoriteReducer.js
@@ -1,9 +1,12 @@
-// reducers must be pure and not mutate original state
+// reducers must be pure (predictable) and not mutate original state
+
 const initialState = []
 
 export default function favoriteReducer(state = initialState, action){
+    // type is required
     switch(action.type){
         case 'FETCH_FAVORITES':
+            // return new state obj, copying over existing state, so prev data isn't lost
             return {
                 ...state,
                 favorites: action.payload
