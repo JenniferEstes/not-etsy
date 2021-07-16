3176833bd4b9ee865fc06c348b4dbb4322418c58
Jennifer <jenndestes@gmail.com>
2021-07-16 01:19:25 -0500

    Added business form route

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -1,7 +1,10 @@
 import React from 'react'
-// switch allows navigation between routes; finds exact route and first route that matches
+// switch looks through children routes and renders the first one that matches the current URL; allowing only 1 to render at a time
+// if no route matches, switch renders null
+// route renders UI when path matches current URL
 import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
+import BusinessForm from './BusinessForm'
 import BusinessList from './BusinessList'
 import CategoryList from './CategoryList'
 import FavoriteList from './FavoriteList'
@@ -12,12 +15,12 @@ export const Router = () => {
     return (
         <Switch>
             <Route exact path='/' component={Home} />
+            <Route path='/businesses/new' component={BusinessForm} />
             <Route path='/businesses' component={BusinessList} />
             <Route path='/categories' component={CategoryList} />
             <Route path='/favorites' component={FavoriteList} />
             <Route path='/products' component={ProductList} />
-            <Route exact path='/about' component={About} />
-
+            <Route path='/about' component={About} />
         </Switch>
     )
 }
