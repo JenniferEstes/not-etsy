2bd8ad95cc3ab70a0fb8d6c986e085b6dae951c7
Jennifer <jenndestes@gmail.com>
2021-07-16 01:26:15 -0500

    Added line breaks between multiple properties for readability

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,29 +6,34 @@ import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 
 export default function Product(props) {
-    const { product, onClickFavorite } = props
+    const { product, onClickFavorite, handleValue} = props
 
     const favorite = (product) => {
         return product.favorite
-            ? <FontAwesomeIcon icon={fasHeart} onClick={onClickFavorite(product)} />
-            : <FontAwesomeIcon icon={farHeart} onClick={onClickFavorite(product)} />
+            ? <FontAwesomeIcon
+                icon={fasHeart}
+                onClick={onClickFavorite(product)}
+            />
+            : <FontAwesomeIcon
+                icon={farHeart}
+                onClick={onClickFavorite(product)}
+            />
     }
 
     return (
         <>
             <Card style={{ width: '18rem' }}>
                 <div className="product-container">
-                    <Card.Img key={product.id} src={product.image} alt=""/>
+                    <Card.Img
+                        key={product.id}
+                        src={product.image}
+                        alt=""/>
                 </div>
                 <Card.Body>
-                    <Row>
-                        <Card.Title>{product.name}</Card.Title>
-                    </Row>
-                    <Row><Card.Subtitle className="mb-2 text-muted">{product.business.name}</Card.Subtitle>
-                    </Row>
-                    <Row>
-                        <Card.Text>${product.price}</Card.Text>
-                    </Row>
+                    <Row><Card.Title>{product.name}</Card.Title></Row>
+                    <Row><Card.Subtitle className="mb-2 text-muted">{product.business.name}</Card.Subtitle></Row>
+                    <Row><Card.Text>${product.price}</Card.Text></Row>
+                    <row><button onClick={() => handleValue(preValue => preValue + 1)}>+</button></row>
                 </Card.Body>
                 {favorite(product)}
             </Card>
--- frontend/src/components/productList.js
+++ frontend/src/components/productList.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector} from "react-redux";
-import { fetchProducts, toggleFavorite } from "../actions/productActions";
-import Product from "./Product";
+import React, { useEffect, useState } from 'react'
+// UseSelector store the return values instead of passing down as props
+// useDispatch dispatches an action to store by adding an action as arg
+import { useDispatch, useSelector} from 'react-redux'
+import { fetchProducts, toggleFavorite } from '../actions/productActions'
+import Product from './Product'
 
 export default function ProductList() {
-    // #first arg is state from store
+    // #first arg is state from store; current state
     // #first products is from rootReducer
     // second products is from productActions
     const productList = useSelector(state => state.products.products)
     const dispatch = useDispatch()
+    const [value, setValue] = useState( 0)
+
 
     useEffect(() => {
         dispatch(fetchProducts())
@@ -17,10 +21,18 @@ export default function ProductList() {
     const favoriteHandler = (product) => () => dispatch(toggleFavorite(product))
 
     return productList != null ? (
-        <div>
+        <>
+            <div>
+                {value}
+            </div>
             {productList.map((p) =>
-                <Product product={p} onClickFavorite={favoriteHandler} key={p.id} />
+                <Product
+                    product={p}
+                    onClickFavorite={favoriteHandler}
+                    key={p.id}
+                    handleValue={setValue}
+                />
             )}
-        </div>
+        </>
     ) : (<div></div>)
 }
