cf9cd9cc3f2401576bf04e6bda8a96b10a93f56d
Jennifer <jenndestes@gmail.com>
2021-07-19 22:06:55 -0500

    Added comments regarding return value of new state obj

--- frontend/src/Reducers/favoriteReducer.js
+++ frontend/src/Reducers/favoriteReducer.js
@@ -3,10 +3,9 @@
 const initialState = []
 
 export default function favoriteReducer(state = initialState, action){
-    // type is required
     switch(action.type){
         case 'FETCH_FAVORITES':
-            // return new state obj, copying over existing state, so prev data isn't lost
+            // return new state obj based on action type, copying over existing state, so prev data isn't lost
             return {
                 ...state,
                 favorites: action.payload
