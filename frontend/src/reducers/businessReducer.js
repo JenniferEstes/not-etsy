3b032bd146ff9cfa70f6d056f87b562da61805d1
Jennifer <jenndestes@gmail.com>
2021-07-19 22:08:26 -0500

    Refactored businesses to not use Redux

--- frontend/src/reducers/businessReducer.js
+++ frontend/src/reducers/businessReducer.js
@@ -1,14 +1,13 @@
-const initialState = []
-
-export default function businessReducer(state = initialState, action){
-    switch(action.type){
-        case 'FETCH_BUSINESSES':
-            return {
-                ...state,
-                businesses: action.payload
-            }
-        default:
-            return state
-    }
-}
-
+// const initialState = []
+//
+// export default function businessReducer(state = initialState, action){
+//     switch(action.type){
+//         case 'FETCH_BUSINESSES':
+//             return {
+//                 ...state,
+//                 businesses: action.payload
+//             }
+//         default:
+//             return state
+//     }
+// }
