c680b3b806ae231cc698f1b9f555f17fe8d2af31
Jennifer <jenndestes@gmail.com>
2021-07-22 01:20:18 -0500

    Deleted BusinessReducer and removed it from rootReducer.js to Complete the refactoring from Redux to React

--- frontend/src/Reducers/rootReducer.js
+++ frontend/src/Reducers/rootReducer.js
@@ -1,13 +1,13 @@
 // dictates what the store is going to look like and how it's going to change
 // must be pure and not mutate original state
 import { combineReducers } from 'redux'
-import businessReducer from './businessReducer'
+// import businessReducer from './businessReducer'
 import categoryReducer from './categoryReducer'
 import favoriteReducer from './favoriteReducer'
 import productReducer from './productReducer'
 
 const rootReducer = combineReducers({
-    businesses: businessReducer,
+    // businesses: businessReducer,
     categories: categoryReducer,
     favorites: favoriteReducer,
     products: productReducer
