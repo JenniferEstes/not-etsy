b5daefd10faed065ea263f054f3e94a210a718c1
Jennifer <jenndestes@gmail.com>
2021-07-22 01:12:38 -0500

    Added helpful comments

--- frontend/src/components/Business.js
+++ frontend/src/components/Business.js
@@ -3,12 +3,14 @@ import BusinessCard from "./BusinessCard";
 
 class Business extends React.Component{
 
+    // props available bc passing business obj into business comp on BusinessList.js'
     state = {
         name: this.props.business.name
     }
 
     render(){
         return(
+            // state is value bc state contains info I want card to display
             <BusinessCard business={this.state} />
         )
     }
