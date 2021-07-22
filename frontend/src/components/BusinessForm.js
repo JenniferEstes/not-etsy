da3add435c6554211b09baaee5232438af1d389c
Jennifer <jenndestes@gmail.com>
2021-07-22 01:19:40 -0500

    Moved post fetch for businesses into BusinessForm.js for readability

--- frontend/src/components/BusinessForm.js
+++ frontend/src/components/BusinessForm.js
@@ -1,13 +1,14 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-export default function BusinessForm(props) {
-
+export default function BusinessForm() {
     const [businessName, setBusinessName] = useState('')
+
+    const onInputChange = e => setBusinessName(e.target.value)
 
     const addToBusinessList = e => {
         e.preventDefault()
         fetch('http://localhost:3000/businesses', {
-            method: 'post',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -16,19 +17,20 @@ export default function BusinessForm(props) {
                     name: businessName
                 }
             })
-        }).then(() => {
-            alert('Added new business: ' + businessName)
-            setBusinessName('')
-        }).catch(err => {
-            console.log('Could not save business: ', err)
         })
+            .then(resp => resp.json())
+            .then(() => {
+                alert('Added new business: ' + businessName)
+                setBusinessName("")
+            })
+            .catch(error => {
+                console.log("Unable to add business. Please try again", error)
+            })
     }
-
-    const onInputChange = e => setBusinessName(e.target.value)
 
     return (
         // controlled form - derives its input values from state
-        <>
+        <div>
             <h3> Add A Business </h3>
             <form onSubmit={addToBusinessList}>
                 <input
@@ -39,6 +41,70 @@ export default function BusinessForm(props) {
                 />
                 <button type="submit">Submit</button>
             </form>
-        </>
+            <br/>
+        </div>
     )
 }
+
+
+// import React, { Component  } from 'react'
+//
+// class BusinessForm extends Component {
+//
+//     state = {
+//         formInput: {
+//             businessName: ""
+//         },
+//         error: null
+//     }
+//
+//     onInputChange = e => {
+//         this.setState({
+//             formInput: {
+//                 ...this.state.formInput,
+//                 businessName: e.target.value
+//             }
+//         })
+//     }
+//
+//     addToBusinessList = (e, formInput) => {
+//         e.preventDefault()
+//         this.props(formInput)
+//     }
+//
+//     componentDidMount() {
+//         fetch('http://localhost:3000/businesses', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//             ,
+//             body: JSON.stringify(this.state.formInput)
+//         })
+//             .then(resp => resp.json())
+//             .then(result => {
+//                     this.setState({
+//                         businessName: result.businessName
+//                     })
+//             })
+//     }
+//
+//     render() {
+//             return (
+//                 <div>
+//                     <h3> Add A Business </h3>
+//                     <form onSubmit={(e) => this.addToBusinessList(e, this.state.formInput)}>
+//                         <input
+//                             type="text"
+//                             placeholder="Business Name"
+//                             value={this.state.formInput.businessName}
+//                             onChange={this.onInputChange}
+//                         />
+//                         <button type="submit">Submit</button>
+//                     </form>
+//                     <br/>
+//                 </div>
+//             )
+//         }
+// }
+// export default BusinessForm
