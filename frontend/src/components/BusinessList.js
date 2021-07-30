c529f46399637e26bb98f06ccc0efb89881ae221
Jennifer <jenndestes@gmail.com>
2021-07-30 02:13:53 -0500

    Refactored functional component into class component

--- frontend/src/components/BusinessList.js
+++ frontend/src/components/BusinessList.js
@@ -1,34 +1,103 @@
-import React, { useEffect, useState } from 'react'
-// import { useDispatch, useSelector} from 'react-redux'
+import React, { Component } from 'react'
 import Business from './Business'
 import BusinessForm from './BusinessForm'
+import Button from './Button'
 
-export default function BusinessList() {
-    // const busList = useSelector( state => state.businesses.businesses)
-    // const dispatch = useDispatch()
+class BusinessList extends Component {
 
-    const [businesses, setBusinesses] = useState([])
+    state = {
+        businesses: []
+    }
 
-    useEffect(() => {
+    componentDidMount() {
         fetch('http://localhost:3000/businesses')
             .then(resp => resp.json())
-            .then(result =>
-            // With empty dependency, you would have to refresh browser to see new business
-            setBusinesses(result)
-            )
-    })
+            .then(result => {
+                this.setState({businesses: result
+                })
+            })
+    }
 
-        const businessObjects = businesses.map((business) =>
-        (<Business
-            key={business.id}
-            business={business}
-        />)
-    )
+    businessObjects = () => {
+        return this.state.businesses.map((business) =>
+            (<Business
+                key={business.id}
+                business={business}
+            />)
+        )
+    }
 
-        return (
-        <>
-            <BusinessForm />
-            {businessObjects}
-        </>
-    )
+    handleUpdate = (updatedBusiness) => {
+        this.setState(prevState => {
+            return {
+                businesses: [...prevState.businesses, updatedBusiness]
+            }
+        })
+    }
+
+    handleClick = () => {
+        this.setState(prevState => {
+            return {
+                businesses: prevState.businesses.sort((a, b) => {
+                    return a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1
+                })
+            }
+        })
+    }
+    render(){
+        return(
+            <div>
+                <BusinessForm handleBusiness={this.handleUpdate}/>
+                <Button handleClick={this.handleClick} />
+                {this.businessObjects()}
+            </div>
+        )
+    }
 }
+
+export default BusinessList
+
+
+
+// import React, { useEffect, useState } from 'react'
+// import Business from './Business'
+// import {Button} from './Button'
+// import BusinessForm from './BusinessForm'
+//
+// export default function BusinessList() {
+//
+//     const [businesses, setBusinesses] = useState([])
+//
+//
+//     useEffect(() => {
+//         fetch('http://localhost:3000/businesses')
+//             .then(resp => resp.json())
+//             .then(result =>
+//             setBusinesses(result)
+//             )
+//     }, [])businessObjects
+//
+//     const businessObjects = businesses.map((business) =>
+//         (<Business
+//             key={business.id}
+//             business={business}
+//         />)
+//     )
+//
+//     const handleClick = () => {
+//         setBusinesses(prevState => [...prevState].sort((a, b) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1))
+//     }
+//
+//     const handleBusiness = (newBusiness) => {
+//         setBusinesses(prevState => [...prevState, newBusiness])
+//     }
+//
+//
+//     return (
+//         <>
+//             <BusinessForm handleBusiness={handleBusiness}/>
+//             <Button handleClick={handleClick} />
+//             {businessObjects}
+//         </>
+//     )
+// }
